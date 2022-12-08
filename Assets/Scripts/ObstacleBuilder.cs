9bc2d5c16a2a230bb49c2f999616e43fd6955d19
Uan Murphy <G00385338@gmit.ie>
2022-12-08 20:00:19 +0000

    Updated ObstacleBuilder.cs

--- Assets/Scripts/ObstacleBuilder.cs
+++ Assets/Scripts/ObstacleBuilder.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,52 @@ public class ObstacleBuilder : MonoBehaviour
 {
     public enum ObstacleState
     {
-        Open, Blocked, 
+        Open, Blocked, Half
+    }
+
+    ObstacleBuilder()
+    {
+        statedObstacles = new Dictionary<ObstacleState, List<Obstacle>>();
+        foreach (ObstacleState state in Enum.GetValues(typeof(ObstacleState))){
+            statedObstacles.Add(state, new List<Obstacle>());
+        }
+        foreach(Obstacle obs in obstacles){
+            statedObstacles[obs.state].Add(obs);
+        }
     }
 
     [Serializable]
-    public struct Obstacle {
-        GameObject prefab;
-
+    public struct Obstacle 
+    {
+        public GameObject prefab;
+        public ObstacleState state;
+        public float length;
     }
 
     [SerializeField]
     private Obstacle[] obstacles;
+
+    private Dictionary<ObstacleState, List<Obstacle>> statedObstacles;
+
+    public Obstacle[] BuildObstacleRow(Obstacle[] lastRow)
+    {
+        List<Obstacle> list;
+
+        return new Obstacle[]{};
+    }
+
+    private (ObstacleState, bool) returnUpdatedState(Obstacle obstacle){
+        switch(obstacle.state){
+            case ObstacleState.Open:{
+                break;
+            }
+            case ObstacleState.Half:{
+                return (ObstacleState.Open, false);
+            }
+            case ObstacleState.Blocked:{
+                break;
+            }
+        }
+        return (ObstacleState.Open, false);
+    }
 }
